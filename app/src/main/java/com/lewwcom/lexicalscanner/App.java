c7038e8d4df5f78e781cdbed846c1e58c5acb877
nggocnn <nhungocnnn2509@gmail.com>
2022-04-04 21:52:21 +0700

    fork from lewwcom

--- app/src/main/java/com/lewwcom/lexicalscanner/App.java
+++ app/src/main/java/com/lewwcom/lexicalscanner/App.java
@@ -9,7 +9,21 @@ public class App {
                 new LexicalScanner(ClassLoader.getSystemResourceAsStream("dfa.dat"));
 
         String input =
-                "identifier 1+1 1-1 1/1 1*1 1<1 1>1 1<=1 1>=1 1==1 1!=1 a&&b a||b i=1 123 .123e+123 \"abc132dfz \nasdffadsf\"";
+                "float exampleFunction(boolean var1, int var2) {\n" +
+                        "\tif(var1) {\n" +
+                        "\t\treturn (float)var2;\n" +
+                        "\t} else {\n" +
+                        "\t\treturn 94e-1;\n" +
+                        "\t}\n" +
+                        "}\n" +
+                        "\n" +
+                        "int main() {\n" +
+                        "\tboolean b = true;\n" +
+                        "\tboolean c = !b;\n" +
+                        "\tint d = 4420;\n" +
+                        "\tfloat r = exampleFunction(c, d);\n" +
+                        "\treturn 0;\n" +
+                        "}";
         System.out.println("Input: " + input);
         scanner.scan(new ByteArrayInputStream(input.getBytes()));
     }
