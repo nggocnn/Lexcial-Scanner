f17863bdc8fdd83a67d0e6cc191e493a88e56887
lewwcom <lewwcom@outlook.com>
2022-04-06 01:27:12 +0000

    Using FileInputStream for input file

--- app/src/main/java/com/lewwcom/lexicalscanner/App.java
+++ app/src/main/java/com/lewwcom/lexicalscanner/App.java
@@ -1,9 +1,11 @@
 package com.lewwcom.lexicalscanner;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Objects;
-
-import static com.google.common.io.Resources.getResource;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -11,15 +13,14 @@ public class App {
         LexicalScanner scanner = new LexicalScanner(dfaStream);
 
         InputStream inputCodeStream = ClassLoader.getSystemResourceAsStream("sample.c");
-        String inputCode = FileUtil.readTextFromFile(inputCodeStream);
 
-        String outputPath = Objects.requireNonNull(App.class.getResource("")).getFile() + "output.txt";
+        String outputPath =
+                Objects.requireNonNull(App.class.getResource("")).getFile() + "output.txt";
         File outputFile = new File(outputPath);
         boolean newFile = outputFile.createNewFile();
         OutputStream outputStream = new FileOutputStream(outputFile);
 
-        System.out.println("Input: " + inputCode);
-        scanner.scan(new ByteArrayInputStream(inputCode.getBytes()), outputStream);
+        scanner.scan(inputCodeStream, outputStream);
     }
 
 }
